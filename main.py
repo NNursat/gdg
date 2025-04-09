2252202c0b4c223707f52143e4238d01c8457e28
NNursat <salau1977@gmail.com>
2025-04-09 22:05:52 +0500

    alpha

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Ping-Pong")
 
 ball = pygame.Rect(WIDTH // 2, HEIGHT // 2, 20, 20)
-paddle1 = pygame.Rect(30, HEIGHT // 2 - 70, 10, 140)
-paddle2 = pygame.Rect(WIDTH - 40, HEIGHT // 2 - 70, 10, 140)
+paddle1 = pygame.Rect(30, HEIGHT // 2 - 70, 10, 100)
+paddle2 = pygame.Rect(WIDTH - 40, HEIGHT // 2 - 70, 10, 100)
 
 #start settings
 
-BALL_SPEED_X, BALL_SPEED_Y = 5, 5
+BALL_SPEED_X, BALL_SPEED_Y = 10, 10
 ball_speed_x = BALL_SPEED_X
 ball_speed_y = BALL_SPEED_Y
 PADDLE_SPEED = 10
@@ -32,9 +32,38 @@ while True:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w] and paddle1.top > 0:
         paddle1.y = paddle1.y - PADDLE_SPEED
-    if keys[pygame.K_w] and paddle1.bottom < HEIGHT:
+    if keys[pygame.K_s] and paddle1.bottom < HEIGHT:
         paddle1.y = paddle1.y + PADDLE_SPEED
     if keys[pygame.K_UP] and paddle2.top > 0:
         paddle2.y = paddle2.y - PADDLE_SPEED
     if keys[pygame.K_DOWN] and paddle2.bottom < HEIGHT:
         paddle2.y = paddle2.y + PADDLE_SPEED
+
+    #gBu}|{eHue M94a
+
+    ball.x = ball.x + ball_speed_x
+    ball.y = ball.y + ball_speed_y
+
+    #oTckok M94a oT cTeH
+
+    if ball.top < 0 or ball.bottom > HEIGHT:
+        ball_speed_y = -ball_speed_y
+    if ball.colliderect(paddle1) or ball.colliderect(paddle2):
+        ball_speed_x = -ball_speed_x
+
+    #TTpoBepka Bblxoga 3a TTpegeJIbl 3kpaHa
+
+    if ball.left <= 0 or ball.right >= WIDTH:
+        ball.x = WIDTH // 2
+        ball.Y = HEIGHT // 2
+        ball_speed_x = BALL_SPEED_X * (-1 if ball.left <= 0 else 1)
+    
+    #OTpucoBka
+
+    screen.fill((0, 0, 0))
+    pygame.draw.ellipse(screen, (255, 255, 255), ball)
+    pygame.draw.rect(screen, (255, 255, 255), paddle1)
+    pygame.draw.rect(screen, (255, 255, 255), paddle2)
+
+    pygame.display.flip()
+    pygame.time.delay(60)
