e354de47f5629c29ef59d5bef4d89d60ecfa1588
NNursat <salau1977@gmail.com>
2025-04-13 02:30:53 +0500

    added score tracker

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 pygame.init()
 
 WIDTH, HEIGHT = 800, 600
+white = (255, 255, 255)
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Ping-Pong")
@@ -12,12 +13,25 @@ ball = pygame.Rect(WIDTH // 2, HEIGHT // 2, 20, 20)
 paddle1 = pygame.Rect(30, HEIGHT // 2 - 70, 10, 100)
 paddle2 = pygame.Rect(WIDTH - 40, HEIGHT // 2 - 70, 10, 100)
 
+font_style = pygame.font.SysFont(None, 25)
+def text_vyvod(text, color, x, y):
+    text = str(text)
+    txxt = font_style.render(text, True, color)
+    screen.blit(txxt, (x,y))
+
+
+
+
 #start settings
 
 BALL_SPEED_X, BALL_SPEED_Y = 10, 10
 ball_speed_x = BALL_SPEED_X
 ball_speed_y = BALL_SPEED_Y
 PADDLE_SPEED = 10
+
+
+score1 = 0
+score2 = 0
 
 #main loop
 while True:
@@ -53,11 +67,23 @@ while True:
 
     #TTpoBepka Bblxoga 3a TTpegeJIbl 3kpaHa
 
-    if ball.left <= 0 or ball.right >= WIDTH:
+    # if ball.left <= 0 or ball.right >= WIDTH:
+    #     ball.x = WIDTH // 2
+    #     ball.y = HEIGHT // 2
+    #     ball_speed_x = BALL_SPEED_X * (-1 if ball.left >= 0 else 1)
+
+    if ball.left <= 0:
+        score2 += 1
         ball.x = WIDTH // 2
-        ball.Y = HEIGHT // 2
-        ball_speed_x = BALL_SPEED_X * (-1 if ball.left <= 0 else 1)
-    
+        ball.y = HEIGHT // 2
+        ball_speed_x = -BALL_SPEED_X
+
+    if ball.right >= WIDTH:
+        score1 += 1
+        ball.x = WIDTH // 2
+        ball.y = HEIGHT // 2
+        ball_speed_x = BALL_SPEED_X
+
     #OTpucoBka
 
     screen.fill((0, 0, 0))
@@ -65,5 +91,10 @@ while True:
     pygame.draw.rect(screen, (255, 255, 255), paddle1)
     pygame.draw.rect(screen, (255, 255, 255), paddle2)
 
+    #text
+
+    text_vyvod(f"Left Player: {score1}", (255,255,255), 20, 20)
+    text_vyvod(f"Right PLayer: {score2}", (255,255,255), WIDTH - 150, 20)
+
     pygame.display.flip()
-    pygame.time.delay(60)
+    pygame.time.delay(10)
